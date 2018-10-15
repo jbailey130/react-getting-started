ff80c551f50012e440961caad25f16278494d4e4
Bailey <jbailey@zaxbys.com>
2018-10-15 14:49:26 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (props) => {
+    return (
+        <div className="App">
+            <hi>Hello World</hi>
+        </div>
+    )
+}
+
+export default App;
